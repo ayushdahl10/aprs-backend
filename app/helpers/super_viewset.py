99f92185e74c22c853235cfb02cfebd0b5e5529a
ayushdahal11@gmail.com <ayushdahal11@gmail.com>
2023-12-27 19:39:29 +0545

    added read only viewset

--- app/helpers/super_viewset.py
+++ app/helpers/super_viewset.py
@@ -1,9 +1,13 @@
-from rest_framework.viewsets import ModelViewSet
+from django.db import transaction
 from rest_framework import status
-from django.db import transaction, IntegrityError
-from helpers.pagination import CustomPagination
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
 from helpers.exceptions import SerializerNotFoundException, NotFoundException
 from helpers.mixins.api_mixins import APIMixin
+from helpers.pagination import CustomPagination
+
+"""base viewset for CRUD operations"""
 
 
 class SuperViewset(APIMixin, ModelViewSet):
@@ -98,3 +102,71 @@ class SuperViewset(APIMixin, ModelViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return self.on_api_success_response(serializer.data, status=201)
+
+
+"""base viewset only read operations"""
+
+
+class ReadOnlyViewSet(ReadOnlyModelViewSet, APIMixin):
+    lookup_field = "iid"
+    list_serializer = None
+    detail_serializer = None
+    pagination_class = CustomPagination
+
+    def _get_default_serializer_class(self):
+        if self.serializer_class:
+            return self.serializer_class
+        raise SerializerNotFoundException()
+
+    def get_serializer_class(self):
+        if self.action.lower() == "list":
+            if not self.list_serializer:
+                return self._get_default_serializer_class()
+            return self.list_serializer
+        if self.action.lower() == "retrieve":
+            if not self.detail_serializer:
+                return self._get_default_serializer_class()
+            return self.detail_serializer
+        return self.get_serializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = kwargs.get("queryset", None)
+        querysets = self.filter_queryset(
+            queryset if queryset is not None else self.get_queryset()
+        )
+        if self.is_paginated:
+            paginator = self.pagination_class()
+            paginated_queryset = paginator.paginate_queryset(
+                querysets, request, view=self
+            )
+            serializer_class = self.get_serializer_class()
+            serializer = serializer_class(
+                paginated_queryset, many=True, context={"request": request}
+            )
+            page_data, record_data = paginator.get_paginated_response(serializer.data)
+            return self.on_api_success_pagination(
+                page_data=page_data,
+                records_data=record_data,
+                status=status.HTTP_200_OK,
+            )
+
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(
+            querysets, many=True, context={"request": request}
+        )
+        return self.on_api_success_response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            instance = self.get_queryset().get(iid=kwargs["iid"])
+        except Exception:
+            raise NotFoundException(
+                message=f"Couldn't find object with iid {kwargs['iid']}.",
+                status_code=status.HTTP_404_NOT_FOUND,
+            )
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(
+            instance,
+            context={"request": self.request},
+        )
+        return self.on_api_success_response(serializer.data, status=status.HTTP_200_OK)
