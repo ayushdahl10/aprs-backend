1ae91bdeecf63c0abeea69aeff50c05f7a9d47ef
ayushdahal11@gmail.com <ayushdahal11@gmail.com>
2024-01-04 19:27:50 +0545

    fix management command issue superadmin

--- app/autho/management/commands/createsuperadmin.py
+++ app/autho/management/commands/createsuperadmin.py
@@ -1,15 +1,13 @@
-import datetime
-
-from django.core.management import BaseCommand
 from django.contrib.auth import get_user_model
-from autho.models import Group
-from helpers.mixins.helper import generate_random_string
-from autho.models import UserDetail,Staff
-from permissions.models import Role
-from permissions.constant import SUPER_ADMIN
-from autho.constant import UserTypeChoices
-from django.utils import timezone
+from django.contrib.auth.models import Group
+from django.core.management import BaseCommand
 from django.db import transaction
+
+from autho.constant import UserTypeChoices
+from autho.models import UserDetail
+from permissions.constant import SUPER_ADMIN
+from permissions.models import Role
+
 
 class Command(BaseCommand):
     """
@@ -20,30 +18,26 @@ class Command(BaseCommand):
 
     def handle(self, **options):
         username = input("Enter the username for the super admin: ")
-        email=input("Enter Email for super admin: ")
-        password=input("Enter password for super admin: ")
+        email = input("Enter Email for super admin: ")
+        password = input("Enter password for super admin: ")
         with transaction.atomic():
             try:
                 group = Group.objects.get(name=SUPER_ADMIN)
             except Role.DoesNotExist:
                 raise self.stderr.write("Super Admin role does not exists")
-            user={
-                "username":username,
-                "email":email,
-                "password":password,
+            user = {
+                "username": username,
+                "email": email,
+                "password": password,
             }
-            userdetail={
-                "user":None,
-                "full_name":email,
-                "user_type":UserTypeChoices.ADMIN,
-                "is_verified":True
+            userdetail = {
+                "user": None,
+                "full_name": email,
+                "user_type": UserTypeChoices.ADMIN,
+                "is_verified": True,
             }
 
-            fields={
-                "user":user,
-                "detail":userdetail,
-                "groups":group
-            }
+            fields = {"user": user, "detail": userdetail, "groups": group}
 
             try:
                 self.create_default_system_user(**fields)
@@ -56,13 +50,13 @@ class Command(BaseCommand):
             print(f"super admin created successfully")
 
     def create_default_system_user(self, **kwargs):
-        user=kwargs.get("user")
-        detail=kwargs.get("detail")
-        groups=kwargs.get("groups")
+        user = kwargs.get("user")
+        detail = kwargs.get("detail")
+        groups = kwargs.get("groups")
         user_model = get_user_model()
         systemuser = user_model.objects.create(**user)
         systemuser.set_password(user.get("password"))
         systemuser.groups.add(groups)
         systemuser.save()
-        detail.update({"user":systemuser})
+        detail.update({"user": systemuser})
         UserDetail.objects.create(**detail)
