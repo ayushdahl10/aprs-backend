2b2ad9ce021964e10735252990e44be7df8b2ef8
ayushdahal11@gmail.com <ayushdahal11@gmail.com>
2024-01-15 14:37:50 +0545

    updated html in chat

--- app/chat/consumer/chat.py
+++ app/chat/consumer/chat.py
@@ -2,6 +2,7 @@
 import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.utils import timezone
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -9,13 +10,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
         print(self.room_name, self.room_group_name)
-        # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     # Receive message from WebSocket
@@ -23,18 +21,30 @@ class ChatConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         username = text_data_json["username"]
-
+        created_at = timezone.datetime.now().strftime("%H:%M")  #
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
-            {"type": "chat.message", "message": message, "username": username},
+            {
+                "type": "chat.message",
+                "message": message,
+                "username": username,
+                "created_at": created_at,
+            },
         )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event["message"]
         username = event["username"]
+        created_at = event["created_at"]
         # Send message to WebSocket
         await self.send(
-            text_data=json.dumps({"message": message, "username": username})
+            text_data=json.dumps(
+                {
+                    "message": message,
+                    "username": username,
+                    "created_at": created_at,
+                }
+            )
         )
